5b0ffdde97593861b30c5edd77da923936797246
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2019-06-03 01:16:45 +0300

    out reference

--- src/main/kotlin/ru/spbstu/wheels/MutableRef.kt
+++ src/main/kotlin/ru/spbstu/wheels/MutableRef.kt
@@ -18,3 +18,14 @@ fun <T> ref(value: T): MutableRef<T> = SimpleMutableRef(value)
 
 operator fun <T> MutableRef<T>.getValue(thisRef: Any?, prop: KProperty<*>) = value
 operator fun <T> MutableRef<T>.setValue(thisRef: Any?, prop: KProperty<*>, value: T) { this.value = value }
+
+class OutRef<T>(): MutableRef<T> {
+    private var option: Option<T> = Option.empty()
+    override var value: T
+        get() = option.getOrElse { throw IllegalStateException("Out value reference not set yet") }
+        set(value) { option = Option.just(value) }
+
+    override fun toString(): String = "OutRef($option)"
+}
+
+fun <T> out(): MutableRef<T> = OutRef()
