4a821b1a1a11cba77acabc002afcd3c8ef46fa7c
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2019-12-25 19:37:45 +0300

    fixes in random , v. 0.0.0.6

--- src/main/kotlin/ru/spbstu/wheels/Random.kt
+++ src/main/kotlin/ru/spbstu/wheels/Random.kt
@@ -2,21 +2,18 @@ package ru.spbstu.wheels
 
 import kotlin.random.Random
 
-fun Random.ints(): Sequence<Int> = sequence { yield(nextInt()) }
-fun Random.doubles(): Sequence<Double> = sequence { yield(nextDouble()) }
-fun Random.longs(): Sequence<Long> = sequence { yield(nextLong()) }
+fun Random.ints(): Sequence<Int> = generateSequence { nextInt() }
+fun Random.doubles(): Sequence<Double> = generateSequence { nextDouble() }
+fun Random.longs(): Sequence<Long> = generateSequence { nextLong() }
 
-fun Random.ints(size: Int): Sequence<Int> = sequence { yield(nextInt()) }.take(size)
-fun Random.doubles(size: Int): Sequence<Double> = sequence { yield(nextDouble()) }.take(size)
-fun Random.longs(size: Int): Sequence<Long> = sequence { yield(nextLong()) }.take(size)
+fun Random.ints(size: Int): Sequence<Int> = ints().take(size)
+fun Random.doubles(size: Int): Sequence<Double> = doubles().take(size)
+fun Random.longs(size: Int): Sequence<Long> = longs().take(size)
 
-fun Random.ints(from: Int, until: Int): Sequence<Int> = sequence { yield(nextInt(from, until)) }
-fun Random.doubles(from: Double, until: Double): Sequence<Double> = sequence { yield(nextDouble(from, until)) }
-fun Random.longs(from: Long, until: Long): Sequence<Long> = sequence { yield(nextLong(from, until)) }
+fun Random.ints(from: Int, until: Int): Sequence<Int> = generateSequence { nextInt(from, until) }
+fun Random.doubles(from: Double, until: Double): Sequence<Double> = generateSequence { nextDouble(from, until) }
+fun Random.longs(from: Long, until: Long): Sequence<Long> = generateSequence { nextLong(from, until) }
 
-fun Random.ints(size: Int, from: Int, until: Int): Sequence<Int> =
-        sequence { yield(nextInt(from, until)) }.take(size)
-fun Random.doubles(size: Int, from: Double, until: Double): Sequence<Double> =
-        sequence { yield(nextDouble(from, until)) }.take(size)
-fun Random.longs(size: Int, from: Long, until: Long): Sequence<Long> =
-        sequence { yield(nextLong(from, until)) }.take(size)
+fun Random.ints(size: Int, from: Int, until: Int): Sequence<Int> = ints(from, until).take(size)
+fun Random.doubles(size: Int, from: Double, until: Double): Sequence<Double> = doubles(from, until).take(size)
+fun Random.longs(size: Int, from: Long, until: Long): Sequence<Long> = longs(from, until).take(size)
