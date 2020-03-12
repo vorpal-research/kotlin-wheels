71dfc7a7e9b4537ce3c79f947c1dce9d7948aa01
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2020-03-12 15:34:15 +0300

    m

--- src/main/kotlin/ru/spbstu/wheels/Hash.kt
+++ src/main/kotlin/ru/spbstu/wheels/Hash.kt
@@ -5,3 +5,27 @@ fun <T> setHashCode(values: Iterable<T>): Int = values.fold(0) { acc, c -> acc +
 
 // == values.toList().hashCode()
 fun <T> orderedHashCode(values: Iterable<T>): Int = values.fold(1) { acc, c -> 31 * acc + c.hashCode() }
+
+fun <A, B> hashCombine(a: A, b: B): Int {
+    var result = a?.hashCode() ?: 0
+    result = 31 * result + (b?.hashCode() ?: 0)
+    return result
+}
+fun <A, B, C> hashCombine(a: A, b: B, c: C): Int {
+    var result = a?.hashCode() ?: 0
+    result = 31 * result + (b?.hashCode() ?: 0)
+    result = 31 * result + (c?.hashCode() ?: 0)
+    return result
+}
+fun <A, B, C, D> hashCombine(a: A, b: B, c: C, d: D): Int {
+    var result = a?.hashCode() ?: 0
+    result = 31 * result + (b?.hashCode() ?: 0)
+    result = 31 * result + (c?.hashCode() ?: 0)
+    result = 31 * result + (d?.hashCode() ?: 0)
+    return result
+}
+fun <T> hashCombine(vararg t: T): Int {
+    var result = 1
+    for(e in t) result = 31 * result + e.hashCode()
+    return result
+}
