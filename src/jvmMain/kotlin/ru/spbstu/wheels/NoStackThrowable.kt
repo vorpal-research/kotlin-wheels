156ac435724a05c6a735f5186a8febf4cc881be8
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2020-09-28 03:38:57 +0300

    NoStackThrowable

--- src/jvmMain/kotlin/ru/spbstu/wheels/NoStackThrowable.kt
+++ src/jvmMain/kotlin/ru/spbstu/wheels/NoStackThrowable.kt
@@ -2,11 +2,11 @@ package ru.spbstu.wheels
 
 actual open class NoStackThrowable
 actual constructor(message: String?, cause: Throwable?) :
-        Throwable(message, cause,
-                enableSuppression = false,
-                writableStackTrace = false) {
+        Throwable(message, cause) {
     actual constructor() : this(null, null)
     actual constructor(message: String) : this(message, null)
     actual constructor(cause: Throwable) : this(cause.message, cause)
+
+    override fun fillInStackTrace(): NoStackThrowable = this
 }
 
